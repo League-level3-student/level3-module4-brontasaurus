36940951c07ac1b1ee5dae42d1cd802f8e100bf7
brontasaurus <58154713+brontasaurus@users.noreply.github.com>
2022-11-06 11:35:07 -0800

    still working but going well. need to fix bugs

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -1,5 +1,89 @@
 package _03_Hangman;
 
-public class Hangman {
+import java.util.ArrayList;
+import java.util.Stack;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Hangman {
+	Stack<String> wordList = new Stack<String>();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel wordLabel = new JLabel();
+	JLabel lifeLabel = new JLabel();
+	JLabel wrongLabel = new JLabel();
+	
+	
+ public static void main(String[] args) {
+	
+	 
+}	
+ 
+ public void start() {
+	 System.out.println("starting");
+	 int life = 6;
+	 boolean playing = true;
+	 
+	 while (playing) {
+	 
+		 
+	 String inputNum = JOptionPane.showInputDialog("Enter a number between 1 and " + Utilities.getTotalWordsInFile("dictionary.txt"));
+	 int numOfWords = Integer.parseInt(inputNum);
+	 
+	
+	 
+	 while (wordList.size() < numOfWords) {
+		String pushed = wordList.push(Utilities.readRandomLineFromFile("dictionary.txt"));
+		if (!wordList.contains(pushed)) {	
+			wordList.push(pushed);
+		}
+	 }
+	 while (life > 0 && wordList.size() > 0) {
+	 String word = wordList.pop();
+	 System.out.println(word);
+	 int wordLength = word.length();
+	 ArrayList<Character> wrongArray = new ArrayList<Character>();
+	 ArrayList<Character> guessArray = new ArrayList<Character>();
+	 //String underscores = word.replace(' ', '_');
+	 for (int i = 0; i < wordLength; i++) {
+		 guessArray.add('_');
+	 }
+	 
+	 frame.add(panel);
+	 panel.add(wordLabel);
+	 panel.add(lifeLabel);
+	 panel.add(wrongLabel);
+	 wrongLabel.setText(wrongArray.toString());
+	 lifeLabel.setText(String.valueOf(life));
+	 wordLabel.setText(guessArray.toString());
+	 frame.setVisible(true);
+	 frame.pack();
+	 
+	 while (life > 0 && guessArray.contains('_')) {
+	 String guess = JOptionPane.showInputDialog("Enter a letter");
+	 
+	 if (word.contains(guess) == true){
+		 int index = word.indexOf(guess);
+		 guessArray.set(index, guess.charAt(0));
+		 wordLabel.setText(guessArray.toString());
+		 frame.pack();
+		 if (!guessArray.contains('_')) {
+			life = 6;
+		 }
+	 }
+	 else {
+		 life--;
+		 wrongArray.add(guess.charAt(0));
+	 }
+	 }
+	 
+ }
+ }
+	 //Make a JLabel+ buttons for yes and no. If yes reset if no shut off. JLabel asking if they want to play again
+	 //does not add wrong letters to new array or take away lives.
+	 //Does not enter double letters
+ }
 }
---
+++ src/_03_Hangman/HangmanRunner.java
@@ -0,0 +1,9 @@
+package _03_Hangman;
+
+public class HangmanRunner {
+public static void main(String[] args) {
+	
+	Hangman hangman =  new Hangman();
+	hangman.start();
+}
+}
